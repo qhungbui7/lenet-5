1bdf41efee9434271494877de2bc7605e7f2ba2b
Quoc Hung Bui <bui.qhung.12.7@gmail.com>
2023-12-25 21:56:56 +0700

    tiled

--- src/custom/tiled/tiled.h
+++ src/custom/tiled/tiled.h
@@ -39,7 +39,7 @@ private:
      void init();
 
 public:
-     Titled(int channel_in, int height_in, int width_in, int channel_out,
+     Tiled(int channel_in, int height_in, int width_in, int channel_out,
                int height_kernel, int width_kernel, int stride = 1, int pad_w = 0,
                int pad_h = 0) : dim_in(channel_in * height_in * width_in),
                                 channel_in(channel_in), height_in(height_in), width_in(width_in),
