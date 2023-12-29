060fd9d88b569f85067b50730c4e90a4eceb5337
Quoc Hung Bui <bui.qhung.12.7@gmail.com>
2023-12-29 22:12:35 +0700

    remove global

--- src/custom/constant-mem/constant_mem.cu
+++ src/custom/constant-mem/constant_mem.cu
@@ -21,15 +21,15 @@
 #define K_MAX   7
 __constant__ float kernel[M_MAX * C_MAX * K_MAX * K_MAX];
 
-__global__ float kernel_flatten(float* kernel, int i3, int i2, int i1, int i0, int C, int K){
+float kernel_flatten(float* kernel, int i3, int i2, int i1, int i0, int C, int K){
   return kernel[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0];
 }
 
-__global__ float input_flatten(float* x, int i3, int i2, int i1, int i0, int C, int H, int W){
+float input_flatten(float* x, int i3, int i2, int i1, int i0, int C, int H, int W){
   return  x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0];
 }
 
-__global__ float output_flatten(float* y, int i3, int i2, int i1, int i0, int H_out, int W_out){
+float output_flatten(float* y, int i3, int i2, int i1, int i0, int H_out, int W_out){
   return y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]; 
 }
 
