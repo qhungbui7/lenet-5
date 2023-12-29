a74f2d046ce47e0b76de8d237d1be06168ca7e55
Quoc Hung Bui <bui.qhung.12.7@gmail.com>
2023-12-30 01:49:07 +0700

    test

--- src/custom/constant-mem/gpu_constant_mem.h
+++ src/custom/constant-mem/gpu_constant_mem.h
@@ -1,7 +1,6 @@
 #ifndef SRC_GPU_RESTRICT_UNROLL_H_
 #define SRC_GPU_RESTRICT_UNROLL_H_
-class GPUConstantMemInterface
-{
+class GPUConstantMemInterface {
 public:
     void device_info();
     void get_data_from_gpu(const float *host_y, const float *host_x, const float *host_k, float **device_y_ptr, float **device_x_ptr, float **device_k_ptr, const int B, const int M, const int C, const int H, const int W, const int K);
